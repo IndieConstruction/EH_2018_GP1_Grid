b909d7e9f483e34435a43cc73c6c8a62d96835c6
Brago <brago@indieconstruction.com>
2018-01-23 14:00:32 +0100

    - Creazione grid.

---
+++ Assets/Script/GridController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour {
+
+    public GameObject TilePrefab;
+
+    public int X = 2;
+    public int Y = 3;
+
+
+
+    private void Start() {
+        CreateGrid();
+    }
+
+    void CreateGrid() {
+
+        float size = TilePrefab.transform.localScale.x + 0.1f;
+
+        for (int x = 0; x < X; x++) {
+            for (int y = 0; y < Y; y++) {
+                
+                Instantiate(TilePrefab, new Vector3(x * size, transform.position.y, y * size), transform.rotation, transform);
+            }
+        }
+    }
+
+}
